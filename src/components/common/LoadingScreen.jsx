6b612d3dc8d2649188d6f38a087e2420a396ec65
aigle-levant <aiglelevant@gmail.com>
2025-08-09 10:49:36 +0530

    Redid loading screen

--- src/components/common/LoadingScreen.jsx
+++ src/components/common/LoadingScreen.jsx
@@ -1,12 +1,13 @@
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import Lottie from "lottie-react";
+import loadingAnimation from "/src/assets/loading.json";
 
 export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
-      <DotLottiePlayer
-        src="/loading.lottie"
-        autoplay
+      <Lottie
+        animationData={loadingAnimation}
         loop
+        autoplay
         className="h-[500px] w-[500px]"
       />
     </div>
