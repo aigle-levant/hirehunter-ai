598618272a6cc3da43641556232c647de540716c
aigle-levant <aiglelevant@gmail.com>
2025-08-16 18:49:39 +0530

    Fixed error in public folder

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ supabase = create_supabase_client()
 # define cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
